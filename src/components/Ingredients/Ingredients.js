29dee73f73ac26350c2696d2165e332f703d9921
Alex Huang <alex.huang@orbitremit.com>
2019-10-17 15:12:00 +1300

    useEffect and useRef applied

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+import IngredientForm from './IngredientForm';
+import Search from './Search';
+import IngredientList from './IngredientList';
+
+function Ingredients() {
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    console.log('rendering ingredients', ingredients);
+  });
+
+  const filteredIngredientsHandler = useCallback(filteredIngredient => {
+    setIngredients(filteredIngredient);
+  }, []);
+
+  const addIngredientHandler = ingredient => {
+    fetch('https://react-hooks-update-f294b.firebaseio.com/ingredients.json', {
+      method: 'Post',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(responseData => {
+        setIngredients(prevIngredients => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
+  };
+  const onRemoveItemHandler = id => {
+    fetch(
+      'https://react-hooks-update-f294b.firebaseio.com/ingredients/${id}.json',
+      {
+        method: 'DELETE',
+      },
+    ).then(response => {
+      setIngredients(prevIngredients =>
+        prevIngredients.filter(ingredient => ingredient.id !== id),
+      );
+    });
+  };
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        <IngredientList
+          ingredients={ingredients}
+          onRemoveItem={onRemoveItemHandler}
+        />
+      </section>
+    </div>
+  );
+}
+
+export default Ingredients;
